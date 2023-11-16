0cfc547eeb4e5605250b5d072309651d8deafbe3
AraRen <jackyccz860511@gmail.com>
2023-11-16 08:57:05 +0800

    mod

--- 2023_11_10/datasource.py
+++ 2023_11_10/datasource.py
@@ -2,81 +2,58 @@ import requests
 import psycopg2
 import password as pw
 
-def __download_PM_data()->list[dict]:
-    '''
-    下載taiwan_pm25
-    https://data.moenv.gov.tw/api/v2/aqx_p_02?language=zh&api_key={pw.APIKEY}
-    '''
-    PM_url = 'https://data.moenv.gov.tw/api/v2/aqx_p_02?language=zh&api_key={pw.APIKEY}'
-    response = requests.get(PM_url)
-    response.raise_for_status()
-    print("連線中，請稍後．．．")
-    return response.json()
+__all__=['update_sqlite_data']
 
-def __create_table(conn)->None:    
+#-----------------download data-----------------#
+def __download_pm25_data()->list[dict]:
+    aqi_url = f'https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key={pw.APIKEY}'
+    response = requests.get(aqi_url)
+    response.raise_for_status()
+    print('下載成功')
+    data = response.json()
+    return data
+
+#---------------create sql table----------------#
+def __create_table(conn)->None:
     cursor = conn.cursor()
     cursor.execute(
         '''
-        CREATE TABLE  IF NOT EXISTS taiwan_pm25(
-            "id"	SERIAL,
-            "測站名稱"	TEXT NOT NULL,
+		CREATE TABLE IF NOT EXISTS taiwan_pm25(
+			"id"	SERIAL,
+            "城市名稱"	TEXT NOT NULL,
             "縣市名稱"	TEXT NOT NULL,
-            "細懸浮微粒濃度"	INTEGER NOT NULL,
-            "資料建置日期"	TEXT NOT NULL,
-            PRIMARY KEY("id"),
-            UNIQUE(測站名稱,資料建置日期) 
-        );
-        '''
+            "pm25"	TEXT NOT NULL,
+            "時間"	TEXT NOT NULL,
+			PRIMARY KEY("id"),
+            UNIQUE(城市名稱,時間)
+		);
+		'''
     )
     conn.commit()
-    cursor.close()
-    print("Table建立中．．．")
+    cursor.close()  
 
+#-----------------insert data-------------------#
 def __insert_data(conn,values:list[any])->None:
     cursor = conn.cursor()
     sql = '''
-    INSERT INTO taiwan_pm25 (測站名稱, 縣市名稱, 細懸浮微粒濃度, 資料建置日期) 
-    VALUES (%s,%s,%s,%s)
-    ON CONFLICT (測站名稱,資料建置日期) DO NOTHING
+        INSERT INTO taiwan_pm25(城市名稱, 縣市名稱, pm25, 時間) 
+        VALUES(%s,%s,%s,%s)
+        ON CONFLICT (城市名稱,時間) DO NOTHING   
     '''
-    cursor.execute(sql,values)    
+    cursor.execute(sql,values)
     conn.commit()
     cursor.close()
 
-def updata_render_data()->None:
-    '''
-    下載,並更新資料庫
-    '''
-    data = __download_PM_data()
+def update_render_data()->None:
+    data = __download_pm25_data()
+
+    #---------------連線到postgresql----------------#
     conn = psycopg2.connect(database=pw.DATABASE,
-                            user=pw.USER, 
-                            password=pw.PASSWORD,
-                            host=pw.HOST, 
-                            port="5432")
-        
+                                user=pw.USER, 
+                                password=pw.PASSWORD, host=pw.HOST, 
+                                port="5432")
+
     __create_table(conn)
-    for item in data:
-        __insert_data(conn,[item['site'],item['county'],item['pm25'],item['datacreationdate']])
+    for item in data['records']: 
+        __insert_data(conn,values=[item['site'],item['county'],item['pm25'],item['datacreationdate']])
     conn.close()
-
-def lastest_datetime_data()->list[tuple]:
-    conn = psycopg2.connect(database=pw.DATABASE,
-                            user=pw.USER, 
-                            password=pw.PASSWORD,
-                            host=pw.HOST, 
-                            port="5432")
-    cursor = conn.cursor()
-    sql = '''
-    SELECT 測站名稱,MAX(資料建置日期) AS 資料建置日期,縣市名稱,細懸浮微粒濃度
-    FROM taiwan_pm25
-    GROUP BY 測站名稱,縣市名稱,資料建置日期,細懸浮微粒濃度
-    '''
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    
-    return rows
-
-
-#https://data.moenv.gov.tw/api/v2/aqx_p_02?language=zh&api_key=0d345e45-9ef9-4614-bdd2-c2adc11b2a5d PM2.5網址
--- 2023_11_10/index.py
+++ 2023_11_10/index.py
@@ -1,56 +1,42 @@
-import datasource
-import psycopg2
-import password as pw
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from PMTreeView import PMTreeView
+import datasource_Lee
 from threading import Timer
 
-
-class Window(tk.Tk):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        topFrame = tk.Frame(self,relief=tk.GROOVE,borderwidth=1)
-        tk.Label(topFrame,text="taiwan_pm25",font=("arial", 20), bg="#333333", fg='#ffffff',padx=10,pady=10).pack(padx=20,pady=20)
-        topFrame.pack(pady=30)
-
-        bottomFrame = tk.Frame(self)
-        
-        self.PMTreeView = PMTreeView(bottomFrame,show="headings",columns=('site','county','pm25','datacreationdate'),height=20)
-        self.PMTreeView.pack(side='left')
-        vsb = ttk.Scrollbar(bottomFrame, orient="vertical", command=self.PMTreeView.yview)
-        vsb.pack(side='left',fill='y')
-        self.PMTreeView.configure(yscrollcommand=vsb.set)
-        bottomFrame.pack(pady=(0,30),padx=20)
-        #-------------------------------------------
-
-
-
-def main():
-    def update_data(w:Window)->None:
+class Window(tk.Tk):                       
+    def __init__(self, **kwargs):          
+        super().__init__(**kwargs)         
         try:
-            datasource.updata_render_data()
+            datasource_Lee.update_render_data()
+        except Exception as e:                     
+            messagebox.showerror("錯誤",f'{e}\n將關閉應用程式\n請稍後再試')
+            self.destroy()                 
 
-        except Exception:
-            messagebox.showerror("錯誤",'網路不正常\n將關閉應用程式\n請稍後再試')
 
-        lastest_data = datasource.lastest_datetime_data()
-        w.PMTreeView.update_content(lastest_data)
+t = None
+def main():
+    def on_closing():
+        print("window關閉")
+        t.cancel()
+        window.destroy()
 
-        t = Timer(5*60, update_data,args=(window,))
-        t.start()
+
+    def update_data()->None:
+        datasource_Lee.update_render_data()
+        global t
+        t = Timer(60*60,update_data)
+        t.start()  
 
     window = Window()
-    window.title('taiwan_pm25')
+    window.title('taiwan_pm2.5')
+    window.geometry('600x300')
     window.resizable(width=False,height=False)
-    lastest_data = datasource.lastest_datetime_data()
-    window.PMTreeView.update_content(lastest_data)
-    t = Timer(1, update_data,args=(window,))
-    t.start()         
+    update_data()
+    window.protocol("WM_DELETE_WINDOW",on_closing)      
     window.mainloop()
-    
-   
 
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     main()
